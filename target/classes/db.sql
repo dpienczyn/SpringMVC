4b8b53d9bfae7840b438d62ca1687515973b0ee4
dpienczyn <donia19881@wp.pl>
2019-03-01 17:43:13 +0100

    zmiany

--- target/classes/db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `pytanie` (
-  `pytanieId` int(11) NOT NULL AUTO_INCREMENT,
-  `opis` varchar(45) NOT NULL,
-  `odp1` varchar(80) NOT NULL,
-  `odp2` varchar(80) NOT NULL,
-  `odp3` varchar(80) NOT NULL,
-  `odppopr` varchar(80) NOT NULL,
-  PRIMARY KEY (`pytanieId`)
-);
